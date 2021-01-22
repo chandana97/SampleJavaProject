ada146f888630b29db74e4912c317aafa4122226
chandana97 <32258450+chandana97@users.noreply.github.com>
2021-01-22 21:41:08 +0530

    Update jenkins.groovy

--- jenkins/jenkins.groovy
+++ jenkins/jenkins.groovy
@@ -1,1 +1,17 @@
 
+node{
+    stage("checkout"){
+        checkout scm
+    }
+    stage("Build"){
+        
+        mavenPath = tool 'maven3'
+        
+        sh "${mavenPath}/mvn command"
+    }
+    
+    stage("RUN"){
+        sh "javac Helloworld.java"
+        sh "java Helloworld"
+    }
+}
